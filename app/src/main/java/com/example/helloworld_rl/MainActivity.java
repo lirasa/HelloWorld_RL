9bc7db67b7a133f37d5dbfdc1316b9f7209fdb72
lirasa <rasaa98@gmail.com>
2024-03-11 18:11:53 +0200

    Revert "Added change text functionality"
    This reverts commit 60c6ed2620b30a0a9b753f65adaeef111a7588fa.

--- app/src/main/java/com/example/helloworld_rl/MainActivity.java
+++ app/src/main/java/com/example/helloworld_rl/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.helloworld_rl;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +10,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +20,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        this.tvMain = findViewById(R.id.tvMain);
-    }
-
-    public void btnChangeTextClick(View view) {
-        this.tvMain.setText("Hello");
     }
 }
